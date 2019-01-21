a20daa913eb2e58c27b1c4c69c6f38327632ebe3
De Leon Rivera <sd18621@essex.ac.uk>
2019-01-21 12:39:38 +0000

    plots

--- labs/lab2/vehicles.py
+++ labs/lab2/vehicles.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 if __name__ == '__main__':
     df = pd.read_csv('./vehicles.csv')
     sns_plot = sns.lmplot(df.columns[0], df.columns[1], data=df, fit_reg=False)
-    sns_plot.savefig("scaterplot.png", bbox_inches='tight')
+    sns_plot.savefig("scatterplot.png", bbox_inches='tight')
+
+    plt.clf()
+    data = df.values.T[1][:79]
+    sns_plot2 = sns.distplot(data, bins=5, kde=False, rug=True).get_figure()
+    sns_plot2.savefig("histogram.png", bbox_inches='tight')
